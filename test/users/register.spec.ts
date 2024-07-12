2ee0c475e2d94d1235f0b2248d64926bf3ea0689
singhbraj <braj333singh@gmail.com>
2024-07-12 22:29:27 +0530

    add database

--- test/users/register.spec.ts
+++ test/users/register.spec.ts
@@ -21,6 +21,46 @@ describe("POST /auth/register", () => {
 
             expect(response.statusCode).toBe(201)
         })
+
+        it("should return valid json response", async () => {
+            //   Arrange
+            const userData = {
+                firstName: "Braj",
+                lastName: "Singh",
+                email: "braj333singh@gmail.com",
+                password: "secret",
+            }
+            //   ACT
+
+            const response = await request(app)
+                .post("/auth/register")
+                .send(userData)
+
+            //   Assert  application/json/utf-8
+
+            expect(response.headers["content-type"]).toEqual(
+                expect.stringContaining("json"),
+            )
+        })
+
+        it("should persist the user in database", async () => {
+            //   Arrange
+            const userData = {
+                firstName: "Braj",
+                lastName: "Singh",
+                email: "braj333singh@gmail.com",
+                password: "secret",
+            }
+            //   ACT
+
+            const response = await request(app)
+                .post("/auth/register")
+                .send(userData)
+
+            //   Assert
+
+            expect(response.statusCode).toBe(201)
+        })
     })
 
     describe("Fields are missing", () => {})
