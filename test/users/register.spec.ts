9b1a0e25b3fe80ef11933b2271ae2e377a7ccb08
singhbraj <braj333singh@gmail.com>
2024-07-13 17:00:10 +0530

    typeorm added

--- test/users/register.spec.ts
+++ test/users/register.spec.ts
@@ -1,7 +1,26 @@
 import request from "supertest"
 import app from "../../src/app"
+import { User } from "../../src/entity/User"
+import { AppDataSource } from "../../src/config/data-source"
+import { DataSource } from "typeorm"
+import { truncateTables } from "../utils"
 
 describe("POST /auth/register", () => {
+    let connection: DataSource
+
+    beforeAll(async () => {
+        connection = await AppDataSource.initialize()
+    })
+
+    beforeEach(async () => {
+        // Database truncate
+        await truncateTables(connection)
+    })
+
+    afterAll(async () => {
+        await connection.destroy()
+    })
+
     describe("Given all fields", () => {
         it("should return the 201 status code", async () => {
             //   Arrange
@@ -53,13 +72,16 @@ describe("POST /auth/register", () => {
             }
             //   ACT
 
-            const response = await request(app)
-                .post("/auth/register")
-                .send(userData)
+            await request(app).post("/auth/register").send(userData)
 
             //   Assert
 
-            expect(response.statusCode).toBe(201)
+            const userRepository = connection.getRepository(User)
+            const users = await userRepository.find()
+            expect(users).toHaveLength(1)
+            // expect(users[0].firstName).toBe(userData.firstName)
+            // expect(users[0].lastName).toBe(userData.lastName)
+            // expect(users[0].email).toBe(userData.email)
         })
     })
 
