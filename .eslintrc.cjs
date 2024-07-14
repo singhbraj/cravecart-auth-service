aed649c8fcee0f92ac66ecd1717c0bfb38a0d732
singhbraj <braj333singh@gmail.com>
2024-07-14 11:57:05 +0530

    add error handling and logging

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,7 +12,7 @@ module.exports = {
     },
     root: true,
     rules: {
-        "no-console": "error",
+        // "no-console": "error",
         "dot-notation": "error",
         "@typescript-eslint/no-misused-promises": "off",
         "@typescript-eslint/require-await": "off",
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -2,12 +2,26 @@ import { Repository } from "typeorm"
 import { AppDataSource } from "../config/data-source"
 import { User } from "../entity/User"
 import { UserData } from "../types"
+import createHttpError from "http-errors"
 
 export class UserService {
     constructor(private userRepository: Repository<User>) {}
 
     async create({ firstName, lastName, email, password }: UserData) {
-        const userRepository = AppDataSource.getRepository(User)
-        await userRepository.save({ firstName, lastName, email, password })
+        try {
+            const userRepository = AppDataSource.getRepository(User)
+            return await userRepository.save({
+                firstName,
+                lastName,
+                email,
+                password,
+            })
+        } catch (err) {
+            const error = createHttpError(
+                500,
+                "Failed to store the data in the database",
+            )
+            throw error
+        }
     }
 }
