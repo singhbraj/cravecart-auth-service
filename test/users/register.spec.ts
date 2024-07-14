0ab4a28c9800b1566ff6f7c23bb5e14925ba2b31
singhbraj <braj333singh@gmail.com>
2024-07-14 12:45:57 +0530

    add role

--- test/users/register.spec.ts
+++ test/users/register.spec.ts
@@ -3,7 +3,7 @@ import app from "../../src/app"
 import { User } from "../../src/entity/User"
 import { AppDataSource } from "../../src/config/data-source"
 import { DataSource } from "typeorm"
-import { truncateTables } from "../utils"
+import { Roles } from "../../src/constants"
 
 describe("POST /auth/register", () => {
     let connection: DataSource
@@ -14,7 +14,9 @@ describe("POST /auth/register", () => {
 
     beforeEach(async () => {
         // Database truncate
-        await truncateTables(connection)
+        await connection.dropDatabase()
+        await connection.synchronize()
+        // await truncateTables(connection)
     })
 
     afterAll(async () => {
@@ -105,6 +107,25 @@ describe("POST /auth/register", () => {
             expect((response.body as Record<string, string>).id).toBe(
                 users[0].id,
             )
+        })
+
+        it("should assign a customer role", async () => {
+            //   Arrange
+            const userData = {
+                firstName: "Braj",
+                lastName: "Singh",
+                email: "braj333singh@gmail.com",
+                password: "secret",
+            }
+            //   ACT
+
+            await request(app).post("/auth/register").send(userData)
+
+            //   Assert
+            const userRepository = connection.getRepository(User)
+            const users = await userRepository.find()
+            expect(users[0]).toHaveProperty("role")
+            expect(users[0].role).toBe(Roles.CUSTOMER)
         })
     })
 
