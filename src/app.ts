6557d0a3b2f7085e1c1c2a15da0a8340a43e6195
singhbraj <braj333singh@gmail.com>
2024-07-26 23:17:37 +0530

    add 401 status code test

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
+app.use(express.static("public"))
+
 app.use(express.json())
 app.use(cookieParser())
 
@@ -20,7 +22,7 @@ app.use("/auth", authRouter)
 app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
     //  if(err instanceof Error){
     logger.error(err.message)
-    const statusCode = err.statusCode || 500
+    const statusCode = err.statusCode || err.status || 500
     res.status(statusCode).json({
         errors: [
             {
